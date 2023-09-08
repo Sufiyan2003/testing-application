cbe4ed706a5441bc00e4b7dacf77249845b8a5e3
sufiyanIntern <sufiyan-intern@sentinel-ai.io>
2023-09-08 09:48:13 +0500

    empty pages added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './component/Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./component/Header";
+import Home from "./FrontEnd/Home";
+import About from "./FrontEnd/About";
+import Skills from "./FrontEnd/Skills";
+import Experience from "./FrontEnd/Experience";
+import Contact from "./FrontEnd/Contact";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <Home />
+      <About />
+      <Skills />
+      <Experience />
+      <Contact />
     </div>
   );
 }
