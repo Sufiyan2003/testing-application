1b4fb8f0d045b8a2db619957bce53d2087276733
sufiyanIntern <sufiyan-intern@sentinel-ai.io>
2023-09-10 10:16:56 +0500

    format the about properly

--- src/FrontEnd/About.js
+++ src/FrontEnd/About.js
@@ -1,12 +1,24 @@
 import React from "react";
 import me from "../pictures/me.jpg";
 import "../assets/About.css";
-import { Row, Col } from "reactstrap";
+import { Row, Col, Button } from "reactstrap";
 
 function About() {
   return (
     <div className="about-container">
-      This is About
+      <div className="image-container">
+        <img src={me} alt="hero" className="actual-image" />
+      </div>
+      <div className="text-container">
+        <p>
+          Hi, my name is Sufiyan Sadiq and welcome to my website, this is simply
+          to test out how far i can push my react skills and maybe make this
+          into my online cv, I have a background in electrical engineering and i
+          am enthusiastic about hardware design and recently i am diving into
+          the deep world of programming. Contact me if you have any question.
+        </p>
+        <Button placeholder="Contact Me" color="red">Contact Me</Button>
+      </div>
     </div>
   );
 }
