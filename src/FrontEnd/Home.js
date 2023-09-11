cf9c576ea3567e634a1c2ad3680d47e8fa8b279f
sufiyanIntern <sufiyan-intern@sentinel-ai.io>
2023-09-11 13:54:14 +0500

    add social credentials in the website

--- src/FrontEnd/Home.js
+++ src/FrontEnd/Home.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { TypeAnimation } from "react-type-animation";
 import "../assets/Home.css";
+import linkedIn from "../pictures/linkedIn.png"
+import facebook from "../pictures/facebook.png"
+import instagram from "../pictures/instagram.png"
+import github from "../pictures/github.png"
 
 function Home() {
   const [titles, setTitles] = useState([
@@ -40,11 +44,33 @@ function Home() {
             "Freelancer",
             2000,
             "Machine Learning Engineer",
-            2000
+            2000,
           ]}
           speed={50}
           repeat={Infinity}
           className="typing"
+        />
+      </div>
+      <div className="social-container">
+        <img 
+          src={linkedIn}
+          className="icon"
+          onClick={() => window.open("https://pk.linkedin.com/in/muhammad-sufiyan-sadiq-169809255", "_blank")}
+        />
+        <img 
+          src={github}
+          className="icon"
+          onClick={() => window.open("https://github.com/Sufiyan2003", "_blank")}
+        />
+        <img 
+          src={instagram}
+          className="icon"
+          onClick={() => window.open("https://www.instagram.com/sufiyansadiq9/", "_blank")}
+        />
+        <img 
+          src={facebook}
+          className="icon"
+          onClick={() => window.open("https://www.facebook.com/profile.php?id=100013317271158", "_blank")}
         />
       </div>
     </div>
