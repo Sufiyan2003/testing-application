54abaaceeef7c8b8fbea8c56f0cde662a1d98e1e
sufiyanIntern <sufiyan-intern@sentinel-ai.io>
2023-09-21 08:42:22 +0500

    add bootstrap

--- src/component/Card.js
+++ src/component/Card.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Card({ title, content }) {
   return (
-    <div className="card shadow-lg 20">
+    <div className="card-container card shadow-lg 20">
       <div className="card-header">{title}</div>
-      <div className="card-body w-50">{content}</div>
+      <div className="card-body">{content}</div>
     </div>
   );
 }
