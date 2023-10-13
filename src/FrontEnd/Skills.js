40cafc9760097a409bdf093e2245bd241e680976
sufiyanIntern <sufiyan-intern@sentinel-ai.io>
2023-10-13 07:33:36 +0500

    added a single progress bar

--- src/FrontEnd/Skills.js
+++ src/FrontEnd/Skills.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "../assets/Skills.css";
+import { Progress } from "reactstrap";
 const Skills = () => {
   return (
-    <div className="skill-container">
+    <div className="skill-container" id="skills">
       <div className="heading">
         <h1>Skills</h1>
       </div>
       <div className="skill-content">
-        <div className="column-1">This is Col 1</div>
-        <div className="column-2">This is Col 2</div>
+        <div className="container">
+          <p>NodeJs</p>
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              aria-valuenow="70"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style={{ width: "70%" }}
+            >
+              70%
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
